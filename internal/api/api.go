34700e53510593e0e8d8986d7fe021d556dcc41d
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-08-07 11:10:04 -0300

    create main go

---
+++ internal/api/api.go
@@ -0,0 +1,40 @@
+package api
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log/slog"
+	"net/http"
+	"sync"
+
+	"github.com/guibafica/AMA_GO_Server/internal/store/pgstore"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+	"github.com/jackc/pgx/v5"
+)
+
+type apiHandler struct {
+	q *pgstore.Queries
+	r *chi.Mux
+}
+
+func (h apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.r.ServeHTTP(w, r)
+}
+
+func NewHandler(q *pgstore.Queries) http.Handler {
+	a := apiHandler{
+		q: q,
+	}
+
+	r := chi.NewRouter()
+
+	a.r = r
+
+	return a
+}
